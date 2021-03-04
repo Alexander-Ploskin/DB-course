09ab4975cf118e2485bdfacd62ed3a9b11fb4f04
Alexander-Ploskin <ploskin0107@gmail.com>
2021-03-04 17:17:15 +0300

    Added unique and not null

--- ChessDB.sql
+++ ChessDB.sql
@@ -2,14 +2,15 @@ CREATE DATABASE chess;
 
 CREATE TABLE chessman(
     id serial primary key,
-    class varchar(20) check (class in ('king', 'queen', 'rook', 'bishop', 'knight', 'pawn')),
-    color varchar(20) check (color in ('white', 'black'))
+    class varchar(20) check (class in ('king', 'queen', 'rook', 'bishop', 'knight', 'pawn')) NOT NULL,
+    color varchar(20) check (color in ('white', 'black')) NOT NULL
 );
 
 CREATE TABLE cell(
     chessman_id int primary key references chessman(id),
-    y char check (y >= 'A' and y <= 'H'),
-    x int check (x >= 1 and x <= 8)
+    y char check (y >= 'A' and y <= 'H') NOT NULL,
+    x int check (x >= 1 and x <= 8) NOT NULL,
+    UNIQUE (x, y)
 );
 
 INSERT INTO chessman(class, color) values ('knight', 'black');
